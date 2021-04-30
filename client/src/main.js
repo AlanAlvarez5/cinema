7b1c0e4f1191db65295e406db2470f027a965667
AlanAlvarez5 <alanalvarez98.aa@gmail.com>
2021-04-29 19:22:41 -0500

    cambios

--- client/src/main.js
+++ client/src/main.js
@@ -9,8 +9,8 @@ import VueAxios from 'vue-axios'
 Vue.use(VueAxios, axios)
 
 // Agregamos la URL base de nuestra API
-axios.defaults.baseURL = 'http://localhost:3000/api';
-// axios.defaults.baseURL = 'http://34.204.81.233:3000/api';
+// axios.defaults.baseURL = 'http://localhost:3000/api';
+axios.defaults.baseURL = 'http://34.204.81.233:3000/api';
 // axios.defaults.baseURL = 'http://34.204.81.233/api';
 
 Vue.config.productionTip = false
